a2a61eda4215085dccde38fb5329b556e658bb01
Nima Hedayati <nimahedayati1380@gmail.com>
2023-11-10 16:14:18 +0330

    Create Banner and Tab components

---
+++ src/components/banner.jsx
@@ -0,0 +1,24 @@
+export default function Banner({ business }) {
+  return (
+    <div className="relative bg-gray-800 py-16 px-6 sm:py-20 sm:px-12 lg:px-16">
+      <div className="absolute inset-0 overflow-hidden">
+        <img
+          src={business.banners[0].image}
+          alt=""
+          className="w-full h-full object-center object-cover"
+        />
+      </div>
+
+      <div
+        aria-hidden="true"
+        className="absolute inset-0 bg-gray-900 bg-opacity-50"
+      />
+
+      <div className="relative max-w-3xl mx-auto flex flex-col items-center text-center">
+        <h2 className="my-8 text-3xl font-extrabold text-white sm:text-4xl">
+          یک تجربه جدید!
+        </h2>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/tab.jsx
@@ -0,0 +1,30 @@
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Tab({ categories }) {
+  return (
+    <div className="top-0 sticky overflow-clip bg-gray-100 border-b border-gray-200 ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:block">
+        <nav className="-mb-px flex space-x-8 space-x-reverse">
+          {categories.map((tab, index) => (
+            <a
+              key={tab.name}
+              href={tab.id}
+              className={classNames(
+                index == 0
+                  ? "border-primary text-primary font-bold"
+                  : "border-transparent font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300",
+                "whitespace-nowrap py-4 px-1 border-b-2 text-base"
+              )}
+              aria-current={tab.current ? "page" : undefined}
+            >
+              {tab.name}
+            </a>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+}
