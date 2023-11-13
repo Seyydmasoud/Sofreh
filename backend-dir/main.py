14dfb2df7bac4004e2ed22b572213e9b825f0db5
Amirhossein Derhami <79319406+AmirhosseinDerhami@users.noreply.github.com>
2023-11-13 19:33:07 +0330

    add docs for functions

--- backend-dir/main.py
+++ backend-dir/main.py
@@ -7,17 +7,31 @@ app = FastAPI()
 
 @app.get("/")
 async def get_root():
+    """
+    basic health check for the main route of the API.
+    :return:JSON object with a single "state" key containing the string value "200 ok"
+    """
     return {"state": "200 ok"}
 
 
 @app.get("/categories")
 async def get_categories_endpoint():
+    """
+    This fetches all category data from the database.
+    This allows the frontend to easily display the list of all available main categories to the user.
+    :return: the categories as a list of Category model objects serialized to JSON.
+    """
     categories = get_categories()
     return [Category(**category) for category in categories]
 
 
 @app.get("/menu")
 async def get_menu_endpoint():
+    """
+    This endpoint fetches all menu item data from the database.
+    This allows the frontend to display the list of all available menu items to build out the menu display for users.
+    :return: It directly returns the list of menu items as a JSON array.
+    """
     items = get_menu_items()
     return items
 
