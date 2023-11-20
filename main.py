4c1c612cdfb23a2fdad7dc118d807bbd1f60ee92
Amirhossein Derhami <79319406+AmirhosseinDerhami@users.noreply.github.com>
2023-11-20 23:58:26 +0330

    fix some bugs

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 
 
 @app.get("/")
+@app.get("/root")
 async def get_root():
     """
     basic health check for the main route of the API.
@@ -17,6 +18,7 @@ async def get_root():
 
 
 @app.get("/categories")
+@app.get("/c")
 async def get_all_categories_endpoint():
     """
     This fetches all category data from the database  .
@@ -27,10 +29,12 @@ async def get_all_categories_endpoint():
 
 
 @app.get("/category/{id_}")
-async def get_category_endpoint(id_: int):
+@app.get("/category")
+@app.get("/c/{id_}")
+async def get_category_endpoint(id_: int = 1):
     """
     This fetch category data from the database by id .
-    :param: id_: The id of the category ,
+    :param: id_: The id of the category .the default is 1 (even if not specified)
     :return: category information as a Category model objects serialized to JSON.
     """
     category_data = get_category(id_)
@@ -38,6 +42,7 @@ async def get_category_endpoint(id_: int):
 
 
 @app.get("/menu")
+@app.get("/m")
 async def get_menu_endpoint():
     """
     This endpoint fetches all menu item data from the database.
